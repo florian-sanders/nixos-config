c3d20e6620a9a14cf2faf1a4b64a22674682ba01
Florian Sanders <sanders.florian@gmail.com>
2024-01-16 20:51:26 +0100

    test

--- hosts/shared/programs/neovim.nix
+++ hosts/shared/programs/neovim.nix
@@ -1,9 +1,17 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-    programs.neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-    };
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    withNodeJs = true;
+    defaultEditor = true;
+
+    extraPackages = with pkgs; [
+      ripgrep
+      libgcc
+      libstdcxx5
+    ];
+  };
 }
